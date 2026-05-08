6531edce7ad21c0e98f1e5eb17e2e1b70b38aa6d
trazll <degleted@gmail.com>
2026-05-08 09:57:40 +0300

    Step 4: Add words data and game logic

--- app/src/main/java/com/shapkin/unscramblegame/ui_model/CameViewModel.kt
+++ app/src/main/java/com/shapkin/unscramblegame/ui_model/CameViewModel.kt
@@ -5,6 +5,7 @@ import com.shapkin.unscramblegame.data.GameUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import com.shapkin.unscramblegame.data.allWords
 
 class CameViewModel: ViewModel() {
     private val _uiState= MutableStateFlow(GameUiState())
@@ -13,6 +14,25 @@ class CameViewModel: ViewModel() {
         resetGame()
     }
     fun resetGame(){
-
+        usedWords.clear()
+        _uiState.value= GameUiState(currentScramdledWord = pickRandomWordAndSuffle())
+    }
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
+    private fun shuffleCurrentWord(word: String): String{
+        val tempWord=word.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord)==word){
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+    private fun pickRandomWordAndSuffle(): String{
+        currentWord = allWords.random()
+        while (usedWords.contains(currentWord)){
+            currentWord=allWords.random()
+        }
+        usedWords.add(currentWord)
+        return shuffleCurrentWord(currentWord)
     }
 }
